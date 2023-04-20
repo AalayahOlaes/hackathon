1a32bedaa09460f7a8c607e2ecdbe29194e94720
Aalayah Olaes <aalayaholaes@Aalayahs-MacBook-Pro-2.local>
2023-04-20 14:27:48 -0400

    does not reload page when serachng again

--- Netflix_searcher.js
+++ Netflix_searcher.js
@@ -11,7 +11,6 @@ searchBar.addEventListener("submit", (event) => {
     const searchTerm = input.value;
     console.log(searchTerm);
     getRequest(searchTerm);
-  
 });
 
 function getRequest(searchTerm) {
@@ -30,26 +29,59 @@ function getRequest(searchTerm) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-
+      let counter = 0;
       const page = document.querySelector("#page");
       // if (data && data.length > 0) {
-        console.log('test data')
+        
         const dataArray = data.results;
-        dataArray.forEach((item) => {
-          if (item.title) {
+        const randomItems = getRandomItems(data.results, 5)
+        for (let i = 0; i < randomItems.length; i++){
+        // dataArray.forEach((item) => {
+          const item = randomItems[i];
+
+          if (counter === 5) break;
+          if (item.title && counter < 5) {
+            
             const fetchedTitle = document.createElement('li');
+            const fetchedYear = document.createElement('p');
+            const fetchedSynopsis = document.createElement('p');
+            // const fetchedImage = document.createElement('img');
+            
+            fetchedYear.innerText = `Release Year: ${item.released}`;
             fetchedTitle.innerText = `Title: ${item.title}`;
+            fetchedSynopsis.innerText = `Synopsis: ${item.synopsis}`;
+            // fetchedImage.src = item.imageurl;
+
+
             page.appendChild(fetchedTitle);
-          } else {
+            page.appendChild(fetchedYear);
+            page.appendChild(fetchedSynopsis)
+            // page.appendChild(fetchedImage);
+            counter++;
+          } 
+          else {
             const noResults = document.createElement("div");
             noResults.innerText = "No results found";
             page.appendChild(noResults);
           }
-        });
+        }
     })
     .catch((error) => {
       console.error(error);
     });
+}
+
+function getRandomItems(items, count) {
+  const randomItems = [];
+  const itemCount = Math.min(count, items.length);
+  while (randomItems.length < itemCount) {
+    const randomIndex = Math.floor(Math.random() * items.length);
+    const randomItem = items[randomIndex];
+    if (randomItems.indexOf(randomItem) === -1) {
+      randomItems.push(randomItem);
+    }
+  }
+  return randomItems;
 }
 
 
